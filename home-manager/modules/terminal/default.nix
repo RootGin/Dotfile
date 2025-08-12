280923f000e96d18879f4ed50e63e2a939796e4e
Gin <131593532+RootGin@users.noreply.github.com>
2025-08-12 08:22:01 +0700

    Update default.nix

--- home-manager/modules/terminal/default.nix
+++ home-manager/modules/terminal/default.nix
@@ -6,21 +6,6 @@ in {
   imports = [
     ./programs
     ./emulators
+#    ./shell
   ];
-
-#   # add environment variables
-#   home.sessionVariables = {
-#     # clean up ~
-#     LESSHISTFILE = "${cache}/less/history";
-#     LESSKEY = "${conf}/less/lesskey";
-#
-#     WINEPREFIX = "${data}/wine";
-#     XAUTHORITY = "$XDG_RUNTIME_DIR/Xauthority";
-#
-#     EDITOR = "hx";
-#     DIRENV_LOG_FORMAT = "";
-#
-#     # auto-run programs using nix-index-database
-#     NIX_AUTO_RUN = "1";
-#   };
 }
